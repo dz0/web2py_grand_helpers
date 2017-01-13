0b858dfa41e2cc7cff58ecea47f178a104afc32b
dz0 <jurgis.pralgauskis@gmail.com>
2017-01-13 11:39:15 +0200

    Update joins_builder.py

--- joins_builder.py
+++ joins_builder.py
@@ -1,1 +1,3 @@
 moved to modules/plugin_joins_builder/   and for examples see in controllers 
+# module: https://github.com/dz0/web2py_grand_helpers/blob/master/app/modules/plugin_joins_builder/joins_builder.py
+# examples: https://github.com/dz0/web2py_grand_helpers/blob/master/app/controllers/plugin_joins_builder.py 
